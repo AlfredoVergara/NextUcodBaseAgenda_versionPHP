b201b20c44a19b8cb4b38e3da165d33d7a97e9ee
Alfredo Vergara <39373323+AlfredoVergara@users.noreply.github.com>
2019-01-17 06:43:30 -0500

    Base de datos

---
+++ agendaphp.sql
@@ -0,0 +1,37 @@
+﻿/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
+/*!40101 SET SQL_MODE='NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
+/*!40103 SET SQL_NOTES='ON' */;
+
+
+DROP DATABASE IF EXISTS `agendaphp`;
+CREATE DATABASE `agendaphp` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `agendaphp`;
+DROP TABLE IF EXISTS `eventos`;
+CREATE TABLE `eventos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `fecha_ini` varchar(255) NOT NULL,
+  `fecha_fin` varchar(255) NOT NULL,
+  `hora_ini` varchar(255) NOT NULL,
+  `hora_fin` varchar(255) NOT NULL,
+  `fk_usuario` varchar(255) NOT NULL,
+  `fulldia` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+INSERT INTO `eventos` VALUES (13,'Entrevista','2019-01-19','2019-01-19','10:30','10:30','Alfredover',0);
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+INSERT INTO `usuarios` VALUES (1,'Alfredover','$2y$10$FXapmn0UClIAvkD7Ht.GKeSvP.qylmqP8m96o5cmZCfDx9KiTnGyi','Alfredo Vergara','1976-07-26');
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
